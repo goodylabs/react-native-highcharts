c7f1ce99806915640ab473a8d2aa3d336d8b8b96
Krzysztof Sokołowski <k.sokolowski@birdnet.pl>
2019-07-05 16:45:59 +0200

    add useWebkit to webview

--- App/index.js
+++ App/index.js
@@ -87,6 +87,7 @@ class ChartWeb extends Component {
                   scalesPageToFit={true}
                   scrollEnabled={false}
                   automaticallyAdjustContentInsets={true}
+                  useWebKit
                   {...this.props}
               />
           </View>
